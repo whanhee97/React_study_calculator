11eaf5f02c984a93e624d27a5fc952986d24cf86
hhlee <whanhee97@naver.com>
2021-04-12 17:56:09 +0900

    delete repeating part by using map

--- src/Calc.js
+++ src/Calc.js
@@ -9,21 +9,20 @@ function Calc({ setHistories }) {
         <div className="calc__keyboard__main">
           <div className="button calc__keyboard__ac">AC</div>
           <div class="calc__keyboard__number">
-            <div class="calc__keyboard__number__line">
-              <div class="button button--blue">1</div>
-              <div class="button button--blue">2</div>
-              <div class="button button--blue">3</div>
-            </div>
-            <div class="calc__keyboard__number__line">
-              <div class="button button--blue">4</div>
-              <div class="button button--blue">5</div>
-              <div class="button button--blue">6</div>
-            </div>
-            <div class="calc__keyboard__number__line">
-              <div class="button button--blue">7</div>
-              <div class="button button--blue">8</div>
-              <div class="button button--blue">9</div>
-            </div>
+            {new Array(3).fill().map((_, lineIdx) => {
+              return (
+                <div key={lineIdx} class="calc__keyboard__number__line">
+                  {new Array(3).fill().map((_, nIdx) => {
+                    const num = nIdx + 1 + lineIdx * 3;
+                    return (
+                      <div key={num} class="button button--blue">
+                        {num}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
           </div>
           <div className="button button--blue--big">0</div>
         </div>
